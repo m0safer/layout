d1c447818d8cac77c29fe4f32c35f896434c6be2
mosafer <foladi169@gmail.com>
2023-12-07 20:41:59 +0330

    5th commit

--- assest/js/app.js
+++ assest/js/app.js
@@ -1,10 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {
-      edge: "right"
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, {
+    edge: "right"
   });
-  var tabs = document.querySelectorAll('.main_ul_navbar')
-  var instance = M.Tabs.init(tabs, {
-     swipeable:true
-  });
+});
+var tabs = document.querySelectorAll('.main_ul_navbar')
+var instance = M.Tabs.init(tabs, {
+  swipeable: true
+});
+var actionbutton = document.querySelectorAll('.fixed-action-btn');
+M.FloatingActionButton.init(actionbutton, {
+  toolbarEnabled: true
+});
